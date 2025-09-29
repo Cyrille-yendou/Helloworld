e8319b679d4a525c27643bfaf335b7b26602b771
unknown <bamenantey@gmail.com>
2025-09-29 10:15:16 +0200

    Premier commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import {tasksCollection} from "./data"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+function App(){
+    const tasks = tasksCollection;
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Todo List</h1>
+      <p>Nombre de tâches: 5</p>
     </>
   )
 }
-
 export default App
